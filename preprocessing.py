5d6a17a8c23d76f81eb205f0b0999091594b1059
alereyesx <63877846+alereyesx@users.noreply.github.com>
2020-08-23 00:45:36 -0400

    Picks training and testing set files

--- preprocessing.py
+++ preprocessing.py
@@ -7,10 +7,12 @@ from scipy.ndimage import median_filter
 from scipy.signal import firwin, convolve
 from wfdb import processing
 from ecgdetectors import Detectors
-import json, csv
+import json, csv, random
 
 NON_USEFUL_ANNOTATIONS = ['~', '|', '+', 'Q']
 ABNORMAL_BEATS = ['F', 'V', 'S']
+FILENAMES = []
+CURRENT_DIRECTORY = os.getcwd()
 
 
 class Beat:
@@ -20,22 +22,6 @@ class Beat:
         self.post_RR = post_RR
         self.local_RR_average = local_RR_average
         self.global_RR_average = global_RR_average
-
-
-def remove_baseline_wander(signal):
-    baseline_wander = median_filter(signal, size=216)  # median filter length 600ms,216 samples
-    baseline_wander = median_filter(signal, size=72)  # median filter length 200ms, 72 samples
-
-    filtered_signal = signal - baseline_wander
-
-    return filtered_signal
-
-
-def remove_powerline_interference(signal):
-    fir_coefficients = firwin(12, 35, fs=360, pass_zero='lowpass')
-    filtered_signal = convolve(fir_coefficients, signal, mode='valid')  # fir filter is actually a convolution
-
-    return filtered_signal
 
 
 def find_r_peak_index(r_peaks, sample_close_to_peak, lower_i, upper_i):
@@ -99,94 +85,143 @@ for filename in os.listdir('D:\\Desarrollo\\databases\\MIT-BIH\\'):
 
         record_p_signal = [sample[0] for sample in record_p_signal]  # downgrading wfdb record to a simple float array
 
-        ### loading wfdb record ##
 
-        ### filtering signal ###
+
+        # filtering signal #
         filtered_signal = np.array(record_p_signal)
-        # filtered_signal = remove_baseline_wander(record_p_signal)
-        # filtered_signal = remove_powerline_interference(filtered_signal)
 
-        ### filtering signal ###
-
-        ### detecting Beat features ###
+        # detecting Beat features #
         qrs_detector = Detectors(signal_frecuency)
 
-        # r_peaks = qrs_detector.pan_tompkins_detector(filtered_signal)
-        # r_peaks = qrs_detector.engzee_detector(filtered_signal)
-        # r_peaks =qrs_detector.two_average_detector(filtered_signal)
-        r_peaks = wfdb.processing.xqrs_detect(sig=filtered_signal, fs=signal_frecuency)
+        # r_peaks = wfdb.processing.xqrs_detect(sig=filtered_signal, fs=signal_frecuency)
+
+
 
         # fifth_minute_r_peak = find_r_peak_index(r_peaks,108000,250,400)
 
         # qrs_sections = get_qrs_sections(r_peaks[fifth_minute_r_peak:],filtered_signal)
 
         # r_peak_counter = fifth_minute_r_peak # first after 5 minutes
-        suma_acumulada = []
-        fill_RR_suma_acumulada(r_peaks, suma_acumulada)
 
-        i = 7
-        end = len(r_peaks) - 1
-        GLOBAL_RR_AVERAGE = int(suma_acumulada[-1] / len(suma_acumulada))
-        beats = []
 
-        # ( filtered_signal[wfdb_annotation.sample[k]] < 0.13 and filtered_signal[wfdb_annotation.sample[k]] > 0 )
-        with open(f"r_peaks vs samples {filename[0]}.txt", 'w') as f:
-            j, k = 0, 0
-            peak_amount = len(wfdb_annotation.sample) if len(wfdb_annotation.sample) < len(r_peaks) else len(r_peaks)
+#         suma_acumulada = []
+#         fill_RR_suma_acumulada(r_peaks, suma_acumulada)
+#
+#         i = 7
+#         end = len(r_peaks) - 1
+#         GLOBAL_RR_AVERAGE = int(suma_acumulada[-1] / len(suma_acumulada))
+#         beats = []
+#
+#         with open(f"{CURRENT_DIRECTORY}\\samples_txt\\r_peaks vs samples {filename[0]}.txt", 'w') as f:
+#             j, k = 0, 0
+#             peak_amount = len(wfdb_annotation.sample) if len(wfdb_annotation.sample) < len(r_peaks) else len(r_peaks)
+#
+#             classified_beats = {
+#                 'id': str(wfdb_record.record_name),
+#                 'normal_beats': [],
+#                 'abnormal_beats': []
+#
+#             }
+#
+#             while j < peak_amount - 2 and k < peak_amount:
+#
+#                 classified_beats_file = open(f'{CURRENT_DIRECTORY}\\json_info\\{filename[0]}.json', 'w')
+#
+#                 if wfdb_annotation.symbol[k] in NON_USEFUL_ANNOTATIONS:
+#                     k += 1
+#                     continue
+#
+#                 if abs(r_peaks[j] - wfdb_annotation.sample[k]) > 11:
+#                     m = k
+#                     while r_peaks[j] - wfdb_annotation.sample[m] > 0 and abs(
+#                             r_peaks[j] - wfdb_annotation.sample[m]) > 11:
+#                         m += 1
+#                     if abs(r_peaks[j] - wfdb_annotation.sample[m]) > 11:
+#                         j += 1
+#                         # k += 1
+#                         continue
+#                     else:
+#                         k = m
+#
+#                 # print(str(r_peaks[j]) + " " + str(wfdb_annotation.sample[j]),file=f)
+#                 if wfdb_annotation.symbol[k] in ABNORMAL_BEATS:
+#                     classified_beats['abnormal_beats'].append(str(r_peaks[j]))
+#
+#                 else:
+#                     classified_beats['normal_beats'].append(str(r_peaks[j]))
+#
+#                 print(str(abs(r_peaks[j] - wfdb_annotation.sample[k])) + " j=" + str(j) +
+#                       " " + str(r_peaks[j]) + " " + str(wfdb_annotation.sample[k]) + " " + wfdb_annotation.symbol[k],
+#                       file=f)
+#                 j += 1
+#                 k += 1
+#
+#         json.dump(classified_beats, classified_beats_file)
+#         classified_beats_file.close()
+#
+#         writer.writerow([classified_beats['id']
+#                             ,
+#                          2000 if len(classified_beats['normal_beats']) > 2000 else len(classified_beats['normal_beats'])
+#                             , len(classified_beats['abnormal_beats'])])
+#
+#         print(f'done, {filename[0]}')
+#
+# classified_csv_beats_file.close()
 
-            classified_beats = {
-                'id': str(wfdb_record.record_name),
-                'normal_beats': [],
-                'abnormal_beats': []
-
-            }
-
-            while j < peak_amount - 2 and k < peak_amount:
-
-                classified_beats_file = open(f'{filename[0]}.json', 'w')
-
-                if wfdb_annotation.symbol[k] in NON_USEFUL_ANNOTATIONS:
-                    k += 1
-                    continue
-
-                if abs(r_peaks[j] - wfdb_annotation.sample[k]) > 11:
-                    m = k
-                    while r_peaks[j] - wfdb_annotation.sample[m] > 0 and abs(
-                            r_peaks[j] - wfdb_annotation.sample[m]) > 11:
-                        m += 1
-                    if abs(r_peaks[j] - wfdb_annotation.sample[m]) > 11:
-                        j += 1
-                        # k += 1
-                        continue
-                    else:
-                        k = m
-
-                # print(str(r_peaks[j]) + " " + str(wfdb_annotation.sample[j]),file=f)
-                if wfdb_annotation.symbol[k] in ABNORMAL_BEATS:
-                    classified_beats['abnormal_beats'].append(str(r_peaks[j]))
-
-                else:
-                    classified_beats['normal_beats'].append(str(r_peaks[j]))
-
-                print(str(abs(r_peaks[j] - wfdb_annotation.sample[k])) + " j=" + str(j) +
-                      " " + str(r_peaks[j]) + " " + str(wfdb_annotation.sample[k]) + " " + wfdb_annotation.symbol[k],
-                      file=f)
-                j += 1
-                k += 1
-
-        json.dump(classified_beats, classified_beats_file)
-        classified_beats_file.close()
-
-        writer.writerow([classified_beats['id']
-                            ,
-                         2000 if len(classified_beats['normal_beats']) > 2000 else len(classified_beats['normal_beats'])
-                            , len(classified_beats['abnormal_beats'])])
-
-        print(f'done, {filename[0]}')
-
-classified_csv_beats_file.close()
+for filename in os.listdir(f"{CURRENT_DIRECTORY}\\json_info\\"):
+    FILENAMES.append(filename)
 
 print('preprocessing complete')
+
+# selecting sets #
+
+cantidad_id_con_anormales_despreciables = 0
+cantidad_items = 0
+training_set_files = []
+testing_set_files = []
+
+
+while cantidad_items < 24:
+    filename = FILENAMES[random.randint(0,len(FILENAMES)) - 1]
+    print(filename)
+
+    with open(f"{CURRENT_DIRECTORY}\\json_info\\{filename}") as file:
+
+        filename_json_info = json.load(file)
+        print(len(filename_json_info['abnormal_beats']))
+
+        if len(filename_json_info['abnormal_beats']) < 5:
+            if cantidad_id_con_anormales_despreciables < 10:
+
+                cantidad_id_con_anormales_despreciables += 1
+                training_set_files.append(filename)
+                cantidad_items += 1
+                FILENAMES.remove(filename)
+                print(FILENAMES)
+        else:
+            training_set_files.append(filename)
+            cantidad_items += 1
+            FILENAMES.remove(filename)
+            print(FILENAMES)
+
+testing_set_files = FILENAMES
+
+print("trainig:" + f"{training_set_files}\n")
+
+training_abnormal_beat_N = 0
+for filename in training_set_files:
+    with open(f"{CURRENT_DIRECTORY}\\json_info\\{filename}") as file:
+        json_info = json.load(file)
+        training_abnormal_beat_N += len(json_info['abnormal_beats'])
+
+print(training_abnormal_beat_N)
+
+print("testing:" + f"{testing_set_files}\n")
+
+
+
+
+
 ### detecting Beat features ###
 
 ### plotting ###
